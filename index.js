68d19113671d21f1a7034ccda42b1d453b8d9d48
Karol Kijowski <karol.kijowski@github.com>
2023-02-08 20:23:33 +0100

    scaled background

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
 
-const canvas = document.querySelector('canvas');
-const c = canvas.getContext('2d');
+canvas.width = 64 * 16
+canvas.height = 64 * 9
 
-canvas.width = 32 * 16;
-canvas.height = 32 * 9;
+const scaledCanvas = {
+    width: canvas.widtj / 4,
+    height: canvas.height / 4
+}
 
-const player = new Player();
+const player = new Player()
 
 const keys = {
     w: {
@@ -28,21 +32,35 @@ const keys = {
     },
 }
 
+const background = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    imageSrc: './img/background.png'
+})
 
 function animate() {
-    window.requestAnimationFrame(animate);
-    c.fillStyle = 'white';
-    c.fillRect(0, 0, canvas.width, canvas.height);
+    window.requestAnimationFrame(animate)
+    c.fillStyle = 'white'
+    c.fillRect(0, 0, canvas.width, canvas.height)
 
-    player.draw();
-    player.update();
+    // scaling background
+    c.save()
+    c.scale(4,4)
+    c.translate(0, -background.image.height + scaledCanvas.height)
+    background.update()
+    c.restore()
+
+    player.draw()
+    player.update()
 
     // moving left and right
     player.velocity.x = 0 //stop movin if key isnt pressed 
     if (keys.d.pressed || keys.ArrowRight.pressed) {
-        player.velocity.x = 3;
+        player.velocity.x = 3
     } else if (keys.a.pressed || keys.ArrowLeft.pressed) {
-        player.velocity.x = -3;
+        player.velocity.x = -3
     }
 
 }
@@ -55,17 +73,17 @@ window.addEventListener('keydown', (event) => {
         case 'ArrowUp':
         case 'w':
             if (player.velocity.y === 0) {
-                player.velocity.y = -10;
+                player.velocity.y = -10
             }
-            break;
+            break
         case 'ArrowRight':
         case 'd':
-            keys.d.pressed = true;
-            break;
+            keys.d.pressed = true
+            break
         case 'ArrowLeft':
         case 'a':
-            keys.a.pressed = true;
-            break;
+            keys.a.pressed = true
+            break
     }
 })
 
@@ -74,11 +92,11 @@ window.addEventListener('keyup', (event) => {
     switch (event.key) {
         case 'ArrowRight':
         case 'd':
-            keys.d.pressed = false;
+            keys.d.pressed = false
             break;
         case 'ArrowLeft':
         case 'a':
-            keys.a.pressed = false;
-            break;
+            keys.a.pressed = false
+            break
     }
 })
--- js/classes/Player.js
+++ js/classes/Player.js
@@ -8,17 +8,17 @@ class Player {
             x: 0,
             y: 0
         }
-        this.width = 50;
-        this.height = 50;
+        this.width = 50
+        this.height = 50
         this.sides = {
             bottom: this.position.y + this.height
         }
 
-        this.gravity = 1;
+        this.gravity = 1
     }
 
     draw() {
-        c.fillStyle = 'red';
+        c.fillStyle = 'red'
         c.fillRect(
             this.position.x,
             this.position.y,
@@ -28,15 +28,15 @@ class Player {
     }
 
     update() {
-        this.position.x += this.velocity.x;
-        this.position.y += this.velocity.y;
-        this.sides.bottom = this.position.y + this.height;
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+        this.sides.bottom = this.position.y + this.height
 
         // above bottom of canvas
         if (this.sides.bottom + this.velocity.y < canvas.height) {
-            this.velocity.y += this.gravity;
+            this.velocity.y += this.gravity
         }else{
-            this.velocity.y = 0;
+            this.velocity.y = 0
         }
     }
 }
